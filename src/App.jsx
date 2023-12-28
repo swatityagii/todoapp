98c50fe610896af07403b5bde8ea9f7156660a64
swati <swatityagi8053@gmail.com>
2023-12-28 14:47:27 +0530

    counterapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setcount] = useState(0);
+  function handleclick() {
+    setcount(count + 1);
+  }
+  function handleclick2() {
+    if (count > 0) {
+      setcount(count - 1);
+    }
+  }
   return (
     <>
-      hello
+      <h1>{count}</h1>
+      <button onClick={handleclick}>increment</button>
+      <button onClick={handleclick2} disabled={!count > 0}>
+        decrement
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
