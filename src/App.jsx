ddbc59e929bd4a3d3483d825ba419774025c2ad5
swati <swatityagi8053@gmail.com>
2023-12-28 19:03:16 +0530

    fix comments and design

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [number, setcount] = useState(0);
+  const [number, setnumber] = useState(0);
   function numberincrease() {
-    setcount(number + 1);
+    setnumber(number + 1);
   }
   function numberdecrease() {
     if (number > 0) {
-      setcount(number - 1);
+      setnumber(number - 1);
     }
   }
   return (
     <>
-      <h1>{number}</h1>
-      <button onClick={numberincrease}>increment</button>
-      <button onClick={numberdecrease} disabled={!number > 0}>
-        decrement
+    <div className="p-[100px]">
+      <h1 className="font-bold text-5xl text-sky-500 mb-7">{number}</h1>
+      <button onClick={numberincrease} className=" text-gray-50 text-3xl font-semibold  mr-8 pt-3 pb-3 pl-4 pr-4 bg-emerald-500 hover:shadow-lg rounded-md">Increment </button>
+      <button onClick={numberdecrease} disabled={!number > 0} className=" text-gray-50 text-3xl font-semibold pt-3 pb-3 pl-4 pr-4 bg-red-500 hover:shadow-lg rounded-md">
+        Decrement
       </button>
+      </div>
     </>
   );
 }
