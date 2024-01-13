1117f2abd5c8a34af11e67f81f19c210d52c680b
swati <swatityagi8053@gmail.com>
2024-01-13 18:52:08 +0530

    remove unnecessary prop

--- src/Components/Badges.jsx
+++ src/Components/Badges.jsx
@@ -1,18 +1,16 @@
 import PropTypes from "prop-types";
 
 function Badges(props) {
-  const { completed, color, label } = props;
+  const {  color, label } = props;
   return (
     <span
       className={`ml-3 rounded-sm pl-1 pr-1 text-white text-sm ${color} cursor-default`}
     >
       {label}
-      {completed}
     </span>
   );
 }
 Badges.propTypes = {
-  completed: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
 };
